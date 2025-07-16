c0b20a44b03c6a0b60ccb3a37dd8f39a2ff3e9f2
JonathanG <jg4824261@gmail.com>
2025-07-16 13:40:38 -0600

    Create settings.js

---
+++ settings.js
@@ -0,0 +1,151 @@
+import { watchFile, unwatchFile } from 'fs' 
+import chalk from 'chalk'
+import { fileURLToPath } from 'url'
+import fs from 'fs'
+import cheerio from 'cheerio'
+import fetch from 'node-fetch'
+import axios from 'axios'
+import moment from 'moment-timezone' 
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+//BETA: Si quiere evitar escribir el número que será bot en la consola, agregué desde aquí entonces:
+//Sólo aplica para opción 2 (ser bot con código de texto de 8 digitos)
+global.botNumber = '' //Ejemplo: 573218138672
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+global.owner = [
+  ['50557333744', '🜲 JonathanG 🜲', true],
+  [''],
+  [''],
+  [''],
+  ['']
+];
+global.lidOwners = [['+128514011471987']] // Nota: esta configuracion es para que el bot reconozca a los owners en los grupos, si no quieres que el bot reconozca a los owners en los grupos, simplemente deja el array vacío: global.lidOwners = []
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+global.mods = []
+global.suittag = ['50557333744']
+global.prems = ['50557333744']
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+global.libreria = 'Baileys'
+global.baileys = 'V 6.7.16' 
+global.languaje = 'Español'
+global.vs = '1.0.0'
+global.nameqr = 'MitaBot-MD'
+global.namebot = 'MitaBot'
+global.sessions = 'Sessions'
+global.jadi = 'JadiBots' 
+global.mitaJadibts = true
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+global.packname = '⪛✰ MitaBot ✰⪜'
+global.botname = 'MitaBot-MD'
+global.wm = ''
+global.author = 'Made with JonathanG'
+global.dev = 'Made with JonathanG'
+global.textbot =  'Mitabot • Made With JonathanG'
+global.etiqueta = 'JonathanG'
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+global.moneda = 'Mita Coins'
+global.welcom1 = '☟︎︎︎ Edita Con El Comando setwelcome'
+global.welcom2 = '☟︎︎︎ Edita Con El Comando setbye'
+global.banner = 'https://files.catbox.moe/xr1spw.jpg'
+global.avatar = 'https://tinyurl.com/258ghupn'
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+global.gp1 = 'https://chat.whatsapp.com/DT2hwaaIKMh10bo6FnDYGO'
+global.comunidad1 = 'https://chat.whatsapp.com/DFM4O5znWumLXnVwEWU3qZ'
+global.channel = 'https://whatsapp.com/channel/0029VawknnCKQuJKpYEYq500'
+global.channel2 = 'https://whatsapp.com/channel/0029VawknnCKQuJKpYEYq500'
+global.md = 'https://github.com/jonathanggg/kennybot'
+global.correo = 'jg4824261@gmail.com'
+global.cn ='https://whatsapp.com/channel/0029VawknnCKQuJKpYEYq500';
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+global.catalogo = fs.readFileSync('./src/catalogo.jpg');
+global.estilo = { key: {  fromMe: false, participant: `0@s.whatsapp.net`, ...(false ? { remoteJid: "5219992095479-1625305606@g.us" } : {}) }, message: { orderMessage: { itemCount : -999999, status: 1, surface : 1, message: packname, orderTitle: 'Bang', thumbnail: catalogo, sellerJid: '0@s.whatsapp.net'}}}
+global.ch = {
+ch1: 'newsletter id',
+}
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+global.cheerio = cheerio
+global.fs = fs
+global.fetch = fetch
+global.axios = axios
+global.moment = moment   
+
+global.rpg = {
+  emoticon(string) {
+    string = string.toLowerCase();
+    const emot = {
+      level: '🌟 Nivel',
+      coin: '💸 Coin',
+      exp: '✨ Experiencia',
+      bank: '🏦 Banco',
+      diamond: '💎 Diamante',
+      health: '❤️ Salud',
+      kyubi: '🌀 Magia',
+      joincount: '💰 Token',
+      emerald: '♦️ Esmeralda',
+      stamina: '⚡ Energía',
+      role: '⚜️ Rango',
+      premium: '🎟️ Premium',
+      pointxp: '📧 Puntos Exp',
+      gold: '👑 Oro',
+      iron: '⛓️ Hierro',
+      coal: '🌑 Carbón',
+      stone: '🪨 Piedra',
+      potion: '🥤 Poción',
+    };
+    const results = Object.keys(emot).map((v) => [v, new RegExp(v, 'gi')]).filter((v) => v[1].test(string));
+    if (!results.length) return '';
+    else return emot[results[0][0]];
+  }};
+global.rpgg = { 
+  emoticon(string) {
+    string = string.toLowerCase();
+    const emott = {
+      level: '🌟',
+      coin: '💸',
+      exp: '✨',
+      bank: '🏦',
+      diamond: '💎',
+      health: '❤️',
+      kyubi: '🌀',
+      joincount: '💰',
+      emerald: '♦️',
+      stamina: '⚡',
+      role: '⚜️',
+      premium: '🎟️',
+      pointxp: '📧',
+      gold: '👑',
+      iron: '⛓️',
+      coal: '🌑',
+      stone: '🪨',
+      potion: '🥤',
+    };
+    const results = Object.keys(emott).map((v) => [v, new RegExp(v, 'gi')]).filter((v) => v[1].test(string));
+    if (!results.length) return '';
+    else return emott[results[0][0]];
+  }};  
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+let file = fileURLToPath(import.meta.url)
+watchFile(file, () => {
+  unwatchFile(file)
+  console.log(chalk.redBright("Update 'settings.js'"))
+  import(`${file}?update=${Date.now()}`)
+})
