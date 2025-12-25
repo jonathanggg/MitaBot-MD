3dd87f2cdc231d035c570a45134ad9e3559c2087
JonathanG <jg4824261@gmail.com>
2025-12-24 22:14:44 -0600

    Actualizar Nano•banana.js

--- plugins/Nano•banana.js
+++ plugins/Nano•banana.js
@@ -4,7 +4,6 @@ import crypto from 'crypto';
 
 const delay = ms => new Promise(r => setTimeout(r, ms));
 
-// Función para generar UUID compatible sin dependencias externas
 const uuidv4 = () => {
     return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
         var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
